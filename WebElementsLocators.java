a7f0842dc0283c7c4099def9eae56fd8fd49c5e9
hendsaid49 <hendsaid49@gmail.com>
2026-05-18 02:35:33 +0300

    Add files via upload

--- WebElementsLocators.java
+++ WebElementsLocators.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
 import org.testng.annotations.Test;
 
 public class WebElementsLocators {
@@ -15,6 +16,15 @@ public class WebElementsLocators {
     By Password = By.xpath("//*[@id=\"_R_clap4jikacppb6amH1_\"]");
     By Showpass = By.cssSelector("[aria-label=\"Show password\"]");
     By Submit = By.xpath("//*[text()='Submit']");
+    By usernamelocator = RelativeLocator.with(By.cssSelector("[name=\"user\"]")).
+            below(By.xpath("//*[text()='Username']"));
+    By passlocator = RelativeLocator.with(By.cssSelector("#inputPassword")).
+            below(By.cssSelector("#inputUsername"));
+    By CheckBoxlocator = RelativeLocator.with(By.cssSelector("[value=\"remember-me\"]")).
+            above(By.cssSelector("#loginButton"));
+    By Submitlocator = RelativeLocator.with(By.xpath("//*[text()='Sign in']")).
+            below(By.cssSelector("[type=\"checkbox\"]"));
+
 
     @Test
     public void locators() {
@@ -37,4 +47,20 @@ public class WebElementsLocators {
         driver.findElement(Submit).click();
 
     }
+
+
+    @Test
+    public void RelativeLocators() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://aa-practice-test-automation.vercel.app/index.html");
+        driver.findElement(usernamelocator).sendKeys("Hend Saeed");
+        driver.findElement(passlocator).sendKeys("123456789");
+        driver.findElement(CheckBoxlocator).click();
+        driver.findElement(Submitlocator).click();
+
+
+    }
+
+
 }
